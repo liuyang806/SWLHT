1a73877824f1f581496fea9e45460d09b97d679d
liuyang806 <37374014+liuyang806@users.noreply.github.com>
2022-05-01 22:11:50 +0800

    swlht

--- utils/metrics.py
+++ utils/metrics.py
@@ -1,13 +1,5 @@
 import numpy as np
 
-def RSE(pred, true):
-    return np.sqrt(np.sum((true-pred)**2)) / np.sqrt(np.sum((true-true.mean())**2))
-
-def CORR(pred, true):
-    u = ((true-true.mean(0))*(pred-pred.mean(0))).sum(0) 
-    d = np.sqrt(((true-true.mean(0))**2*(pred-pred.mean(0))**2).sum(0))
-    return (u/d).mean(-1)
-
 def MAE(pred, true):
     return np.mean(np.abs(pred-true))
 
